de08fa800ecefe929a503daebeaf4b6624dcc1cd
westerwave <neowow@online.de>
2016-03-02 21:53:59 +0100

    load creature names and populate listview

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,13 +1,65 @@
 package controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import entity.Creature;
+import entity.Monster;
+import gui.MainGUI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+import library.LibraryEntry;
 
 public class MainController {
 
+    private static int libEntries = 0;
+    private ObservableList<LibraryEntry> libEntryList;
+
     @FXML
-    ListView<String> libraryList;
+    private ListView<LibraryEntry> libraryList;
+
+    @FXML
+    private Text entryAmountText;
+
+    @FXML
+    public void initialize() {
+	loadData();
+	fillLibrary();
+    }
+
+    private void loadData() {
+	try {
+	    Files.walk(Paths.get(System.getProperty("user.dir") + "\\images")).forEach(filePath -> {
+		if (Files.isRegularFile(filePath) && filePath.toString().contains("PNG")) {
+		    String filePathString = filePath.toString();
+		    String[] filePathSplitBackslash = filePathString.split("\\\\");
+		    String[] fileNameSplitDot = filePathSplitBackslash[filePathSplitBackslash.length - 1].split("\\.");
+		    String creatureName = fileNameSplitDot[0];
+		    MainGUI.creatureList
+			    .add(new Monster(creatureName, filePathSplitBackslash[filePathSplitBackslash.length - 1]));
+		    libEntries++;
+		}
+	    });
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	entryAmountText.setText("Number of entries: " + libEntries);
+    }
+
+    private void fillLibrary() {
+	ArrayList<LibraryEntry> leList = new ArrayList<LibraryEntry>();
+	for (Creature c : MainGUI.creatureList) {
+	    leList.add(new LibraryEntry(c));
+	}
+	libEntryList = FXCollections.observableArrayList(leList);
+	libraryList.setItems(libEntryList);
+    }
 
     @FXML
     protected void newEncounter(ActionEvent event) {
@@ -57,5 +109,11 @@ public class MainController {
     @FXML
     protected void previousTurn(ActionEvent event) {
 	System.out.println("prev");
+    }
+
+    @FXML
+    protected void addNewLibraryEntry(ActionEvent event) {
+	System.out.println("newlib");
+
     }
 }
