fb4eabcac99b553a5b4e72690d98c076f4dd4c3b
westerwave <neowow@online.de>
2016-03-02 21:53:36 +0100

    add name setting to libraryentry

--- src/library/LibraryEntry.java
+++ src/library/LibraryEntry.java
@@ -2,12 +2,22 @@ package library;
 
 import java.io.IOException;
 
+import entity.Creature;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class LibraryEntry extends AnchorPane {
 
-    public LibraryEntry() {
+    private Creature creature;
+
+    @FXML
+    private Text nameText;
+
+    public LibraryEntry(Creature creature) {
+	this.creature = creature;
 	FXMLLoader loader = new FXMLLoader(getClass().getResource("LibraryEntry.fxml"));
 	loader.setRoot(this);
 	loader.setController(this);
@@ -17,6 +27,16 @@ public class LibraryEntry extends AnchorPane {
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
+    }
+
+    @FXML
+    private void initialize() {
+	nameText.setText(creature.getName());
+    }
+
+    @FXML
+    protected void openImage(ActionEvent event) {
+	// TODO open Image
     }
 
 }
