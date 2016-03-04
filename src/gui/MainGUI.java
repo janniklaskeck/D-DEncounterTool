8cb48ebe1d235dce9ea230a133975bcff12e9795
westerwave <neowow@online.de>
2016-03-04 01:55:17 +0100

    add autosavethread

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.util.ArrayList;
 
+import encounter.Encounter;
 import entity.Creature;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +16,12 @@ public class MainGUI extends Application {
     public static final String IMAGEFOLDER = System.getProperty("user.dir") + "\\images\\";
 
     public static ArrayList<Creature> creatureList;
+    public static Encounter encounter;
     public static Stage mainStage;
+
+    private static Thread autoSaveThread;
+    private static final int AUTOSAVEINTERVAL = 60000;
+    private static boolean autosave = false;
 
     public static void main(String[] args) {
 	creatureList = new ArrayList<Creature>();
@@ -28,6 +34,29 @@ public class MainGUI extends Application {
 	Scene scene = new Scene(root, 1280, 720);
 	scene.getStylesheets().add(getClass().getResource("customstyle.css").toString());
 	mainStage = primaryStage;
+	autoSaveThread = new Thread(new Runnable() {
+	    @Override
+	    public void run() {
+		try {
+		    Thread.sleep(5000);
+		} catch (InterruptedException e1) {
+		    e1.printStackTrace();
+		}
+		while (autosave) {
+		    if (encounter.getCreatureList().size() > 0) {
+			encounter.autoSave();
+			System.out.println("AutoSave Complete");
+		    }
+		    try {
+			Thread.sleep(AUTOSAVEINTERVAL);
+		    } catch (InterruptedException e) {
+			e.printStackTrace();
+		    }
+		}
+	    }
+	});
+	autoSaveThread.setDaemon(true);
+	autoSaveThread.start();
 
 	primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
 	primaryStage.setTitle("D&D Encounter Tool v2.0");
