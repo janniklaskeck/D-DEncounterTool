aa58f6a0852ec24d895b39e820d192d7255eaab6
westerwave <neowow@online.de>
2016-03-08 10:29:48 +0100

    alternate image loading from zip file

--- src/library/LibraryEntry.java
+++ src/library/LibraryEntry.java
@@ -1,10 +1,10 @@
 package library;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import entity.Creature;
-import gui.MainGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,56 +21,67 @@ import javafx.stage.Stage;
 
 public class LibraryEntry extends AnchorPane {
 
-    private Creature creature;
-    FXMLLoader loader;
+	private Creature creature;
+	FXMLLoader loader;
 
-    @FXML
-    private Text nameText;
+	@FXML
+	private Text nameText;
 
-    public LibraryEntry(Creature creature) {
-	this.creature = creature;
-	loader = new FXMLLoader(getClass().getResource("LibraryEntry.fxml"));
-	loader.setRoot(this);
-	loader.setController(this);
+	public LibraryEntry(Creature creature) {
+		this.creature = creature;
+		loader = new FXMLLoader(getClass().getResource("LibraryEntry.fxml"));
+		loader.setRoot(this);
+		loader.setController(this);
 
-	try {
-	    loader.load();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    @FXML
-    private void initialize() {
-	nameText.setText(creature.getName());
-    }
-
-    @FXML
-    protected void openImage(ActionEvent event) {
-	final Stage myDialog = new Stage();
-	myDialog.initModality(Modality.WINDOW_MODAL);
-
-	if (creature.getImage() == null) {
-	    try {
-		creature.setImage(
-			new Image(new File(MainGUI.IMAGEFOLDER + creature.getImagePath()).toURI().toString()));
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
+		try {
+			loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	ImageView iv = new ImageView(creature.getImage());
-	VBox vbox = new VBox(new Text(creature.getName()), iv);
-	vbox.setAlignment(Pos.CENTER);
-	vbox.setPadding(new Insets(10));
+	@FXML
+	private void initialize() {
+		nameText.setText(creature.getName());
+	}
 
-	Scene myDialogScene = new Scene(vbox);
+	@FXML
+	protected void openImage(ActionEvent event) {
+		final Stage myDialog = new Stage();
+		myDialog.initModality(Modality.WINDOW_MODAL);
 
-	myDialog.setScene(myDialogScene);
-	myDialog.show();
-    }
+		if (creature.getImage() == null) {
+			/*
+			 * try { creature.setImage( new Image(new File(MainGUI.IMAGEFOLDER +
+			 * creature.getImagePath()).toURI().toString())); } catch (Exception
+			 * e) { e.printStackTrace(); }
+			 */
+			setImage();
+		}
 
-    public Creature getCreature() {
-	return creature;
-    }
+		ImageView iv = new ImageView(creature.getImage());
+		VBox vbox = new VBox(new Text(creature.getName()), iv);
+		vbox.setAlignment(Pos.CENTER);
+		vbox.setPadding(new Insets(10));
+
+		Scene myDialogScene = new Scene(vbox);
+
+		myDialog.setScene(myDialogScene);
+		myDialog.show();
+	}
+
+	public Creature getCreature() {
+		return creature;
+	}
+
+	private void setImage() {
+		try {
+			ZipFile zf = new ZipFile(System.getProperty("user.dir") + "\\image.zip");
+			ZipEntry ze = zf.getEntry(creature.getImagePath());
+			creature.setImage(new Image(zf.getInputStream(ze)));
+			zf.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
