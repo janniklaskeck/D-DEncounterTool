0ed469724852a2c8a1b2360a298fd72f6df9208f
Jan-Niklas Keck <janniklaskeck@online.de>
2017-08-26 15:29:59 +0200

    fix library filter

--- src/main/java/app/bvk/library/LibraryGui.java
+++ src/main/java/app/bvk/library/LibraryGui.java
@@ -1,6 +1,8 @@
 package app.bvk.library;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +34,8 @@ public class LibraryGui extends BorderPane
     private ListView<LibraryEntry> libraryList;
 
     private FXMLLoader loader;
+    private final List<LibraryEntry> libraryEntries = new ArrayList<>();
+    private ObservableList<LibraryEntry> libraryEntryDisplayList;
 
     public LibraryGui()
     {
@@ -53,30 +57,39 @@ public class LibraryGui extends BorderPane
     private void initialize()
     {
         this.addLibraryEntryButton.setOnAction(event -> Utils.newLibraryEntryWindow(this.getScene().getWindow()));
-        this.addLibraryTextFieldFilter();
         CreatureLibrary.getInstance();
-        final ObservableList<LibraryEntry> leList = FXCollections.observableArrayList();
-        for (final Creature c : CreatureLibrary.getInstance().getCreatures())
+        for (final Creature creature : CreatureLibrary.getInstance().getCreatures())
         {
-            leList.add(new LibraryEntry(c));
+            this.libraryEntries.add(new LibraryEntry(creature));
         }
+        this.libraryEntryDisplayList = FXCollections.observableArrayList(this.libraryEntries);
+        this.libraryList.setItems(this.libraryEntryDisplayList);
+        this.addLibraryTextFieldFilter();
 
-        this.libraryList.setItems(leList);
+        this.setEntryAmountText();
     }
 
     private void addLibraryTextFieldFilter()
     {
-        final FilteredList<LibraryEntry> fData = new FilteredList<>(this.libraryList.getItems(), p -> true);
+        final FilteredList<LibraryEntry> filteredData = new FilteredList<>(this.libraryList.getItems(), predicate -> true);
         this.filterTextField.textProperty().addListener((obs, oldValue, newValue) ->
         {
-            fData.setPredicate(t ->
+            filteredData.setPredicate(libraryEntry ->
             {
                 final boolean isEmpty = newValue == null || newValue.isEmpty();
-                final boolean nameMatch = t.getCreature().getName().get().toLowerCase().contains(newValue.toLowerCase());
+                final String creatureName = libraryEntry.getCreature().getName().get().toLowerCase();
+                final String filterValue = newValue.toLowerCase();
+                final boolean nameMatch = creatureName.contains(filterValue);
                 return isEmpty || nameMatch;
             });
-            this.libraryList.setItems(fData);
+            this.libraryList.setItems(filteredData);
+            this.setEntryAmountText();
         });
+    }
+
+    private void setEntryAmountText()
+    {
+        this.entryAmountText.setText(String.format("# %d", this.libraryList.getItems().size()));
     }
 
 }
